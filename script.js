dfaa12083246e4f396bdc08e9fa1485cffcd0964
Om1X <glomix@inbox.ru>
2017-02-25 19:19:11 +0300

    select check added

--- script.js
+++ script.js
@@ -1,51 +1,27 @@
 (function ($) {
-  $.datepicker.regional['ru'] = {
-    closeText: 'Закрыть',
-    prevText: '&#x3c;Пред',
-    nextText: 'След&#x3e;',
-    currentText: 'Сегодня',
-    monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
-      'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-    monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн',
-      'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
-    dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
-    dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
-    dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-    weekHeader: 'Не',
-    dateFormat: 'dd-mm-yy',
-    firstDay: 1,
-    isRTL: false,
-    showMonthAfterYear: false,
-    yearSuffix: ''
-  };
-  $.datepicker.setDefaults($.datepicker.regional['ru']);
-
-  $('#birth').datepicker();
-  function onAjaxSuccess(data) {
-    if (data.result) {
-      $('input[type!=button], select, textarea').removeClass().addClass('success');
-      $('.errorBlock').css('display', 'none').html('');
-    } else {
-      errors = data.error;
-      console.log(errors);
-      $('input[type!=button], select, textarea').removeClass().addClass('success');
-      $('.errorBlock').css('display', 'none').html('');
-      $.each(errors, function (key, val) {
-        key = '#' + key.toLowerCase().replace(' ', '_');
-        $(key).removeClass().addClass('error');
-        $('#error').html(val).dialog();
-        $(key).effect('bounce');
-      });
-    }
-  }
-
   $('#submit').on('click', function () {
-    var request = $('form').serializeArray();
+    var obj = $('form').serializeArray();
     $.post(
       'validator.php',
-      request,
+      obj,
       onAjaxSuccess,
       'json'
     );
+
+    function onAjaxSuccess(data) {
+      if (data.result == true) {
+        $('input[type!=button], select, textarea').removeClass().addClass('success');
+        $('.errorBlock').css('display', 'none').html('');
+      } else {
+        requestObj = data.error;
+        $('.errorBlock').css('display', 'none').html('');
+        $('input[type!=button], textarea').removeClass().addClass('success');
+        $.each(requestObj, function (i, val) {
+          if (i === 'Credit Card') i = 'credit_card';
+          $('#' + i.toLowerCase()).removeClass().addClass('error');
+          $('#' + i.toLowerCase() + ' ~ div.errorBlock').css('display', 'block').html(val);
+        });
+      }
+    }
   });
 })(jQuery);
